9cc33c981ef7cde1454d1e3a72fec5413f1f909d
TapoGrill <tapo@students.zcu.cz>
2025-12-06 15:47:37 +0100

    Fully functional basic app no frontend done

--- WalletApp/Services/UserState.cs
+++ WalletApp/Services/UserState.cs
@@ -4,8 +4,19 @@ namespace WalletApp.Services;
 
 public class UserState {
     public User? CurrentUser { get; private set; }
+    
+    public event Action? OnChange;
 
     public void SetUser(User user) {
         CurrentUser = user;
+        NotifyStateChanged();
     }
+
+    public void LogoutUser() {
+        CurrentUser = null;
+        NotifyStateChanged();
+    }
+    
+    // Helper function for event
+    private void NotifyStateChanged() => OnChange?.Invoke();
 }
